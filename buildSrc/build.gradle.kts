029e6b6d33e6461e6f575febcf61b913239da781
toliner <toliner.rkt@gmail.com>
2020-02-24 22:03:43 +0900

    move dependency notation to buildSrc

---
+++ buildSrc/build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    `embedded-kotlin`
+}
+
+group = "SocialHub"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+}
---
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -0,0 +1,22 @@
+object Dependencies {
+    const val JSR_305 = "com.google.code.findbugs:jsr305:3.0.2"
+    const val COMMONS_LANG = "org.apache.commons:commons-lang3:3.7"
+    const val COMMONS_CODEC = "commons-codec:commons-codec:1.11"
+    const val GSON = "com.google.code.gson:gson:2.8.2"
+    const val SCRIBE = "org.scribe:scribe:1.3.3"
+
+    const val JLOGGER = "com.github.uakihir0:JLogger:1.1"
+    const val JHTTP_CLIENT = "com.github.uakihir0:JHttpClient:1.1.5"
+
+    const val TWITTER4J_CORE = "com.github.uakihir0.twitter4j:twitter4j-core:4.0.8.4"
+    const val TWITTER4J_STREAM = "com.github.uakihir0.twitter4j:twitter4j-stream:4.0.8.4"
+    const val TWITTER_TEXT = "com.github.uakihir0:twitter-text:3.0.1.1"
+    const val FACEBOOK4J = "com.github.uakihir0.facebook4j:facebook4j-core:2.4.11.3"
+    const val JSLACK_CLIENT = "com.github.uakihir0.jslack:jslack-api-client:3.2.2.2"
+    const val JSLACK_MODEL = "com.github.uakihir0.jslack:jslack-api-model:3.2.2.2"
+    const val MSINSTANCEJ = "com.github.uakihir0:msinstance4j:0.0.3"
+    const val MASTODON4J = "com.github.uakihir0:mastodon4j:0.2.32"
+    const val JUMBLR = "com.github.uakihir0:jumblr:0.0.13.10"
+
+    const val JUNIT = "junit:junit:4.12"
+}
